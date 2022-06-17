fbaea84d66eb72ac227bf338eb7d0b3d5788c096
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-06-17 16:26:08 -0300

    Cria switch case em alloc_mem

--- servers/pm/alloc.c
+++ servers/pm/alloc.c
@@ -88,29 +88,56 @@ phys_clicks clicks;		/* amount of memory requested */
   phys_clicks old_base;
 
   do {
-        prev_ptr = NIL_HOLE;
-	hp = hole_head;
-	while (hp != NIL_HOLE && hp->h_base < swap_base) {
-		if (hp->h_len >= clicks) {
-			/* We found a hole that is big enough.  Use it. */
-			old_base = hp->h_base;	/* remember where it started */
-			hp->h_base += clicks;	/* bite a piece off */
-			hp->h_len -= clicks;	/* ditto */
+    prev_ptr = NIL_HOLE;
+    hp = hole_head;
+    switch (aloc_strat) {
+        case 0: /* FIRST_FIT */
+            while (hp != NIL_HOLE && hp->h_base < swap_base) {
+                if (hp->h_len >= clicks) {
+                    /* We found a hole that is big enough.  Use it. */
+                    old_base = hp->h_base;	/* remember where it started */
+                    hp->h_base += clicks;	/* bite a piece off */
+                    hp->h_len -= clicks;	/* ditto */
 
-			/* Remember new high watermark of used memory. */
-			if(hp->h_base > high_watermark)
-				high_watermark = hp->h_base;
+                    /* Remember new high watermark of used memory. */
+                    if(hp->h_base > high_watermark)
+                        high_watermark = hp->h_base;
 
-			/* Delete the hole if used up completely. */
-			if (hp->h_len == 0) del_slot(prev_ptr, hp);
+                    /* Delete the hole if used up completely. */
+                    if (hp->h_len == 0) del_slot(prev_ptr, hp);
 
-			/* Return the start address of the acquired block. */
-			return(old_base);
-		}
+                    /* Return the start address of the acquired block. */
+                    return(old_base);
+                }
 
-		prev_ptr = hp;
-		hp = hp->h_next;
-	}
+                prev_ptr = hp;
+                hp = hp->h_next;
+            }
+            break;
+        case 1: /* FIRST_FIT */
+            while (hp != NIL_HOLE && hp->h_base < swap_base) {
+                if (hp->h_len >= clicks) {
+                    /* We found a hole that is big enough.  Use it. */
+                    old_base = hp->h_base;	/* remember where it started */
+                    hp->h_base += clicks;	/* bite a piece off */
+                    hp->h_len -= clicks;	/* ditto */
+
+                    /* Remember new high watermark of used memory. */
+                    if(hp->h_base > high_watermark)
+                        high_watermark = hp->h_base;
+
+                    /* Delete the hole if used up completely. */
+                    if (hp->h_len == 0) del_slot(prev_ptr, hp);
+
+                    /* Return the start address of the acquired block. */
+                    return(old_base);
+                }
+
+                prev_ptr = hp;
+                hp = hp->h_next;
+            }
+            break;
+    }
   } while (swap_out());		/* try to swap some other process out */
   return(NO_MEM);
 }
