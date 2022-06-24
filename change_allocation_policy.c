5c6747523927b370872b764ff6fe5b7608a009c9
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-06-24 16:57:28 -0300

    Adiciona funcionalidade restante de change_allocation_policy

--- change_allocation_policy.c
+++ change_allocation_policy.c
@@ -1,11 +1,39 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(void) 
-{
-    int r;
-    r = changealoc(NEXT_FIT);
-    printf("Aloc strat is %d\n", r);
-    return 0;
+int get_aloc_strategy(const char *strategy_string);
+
+int main(int argc, char *argv[]) {
+  int strategy, result;
+  if (argc < 2) {
+    printf("Argumentos insuficientes\n");
+    return EXIT_FAILURE;
+  }
+  strategy = get_aloc_strategy(argv[1]);
+  if (strategy == -1) {
+    printf("Argumento invalido\n");
+    return EXIT_FAILURE;
+  }
+  result = changealoc(strategy);
+  if (result < 0 || result > 4) {
+    printf("Nao foi possivel alterar a estrategia\n");
+    return EXIT_FAILURE;
+  }
+  printf("Estrategia alterada com sucesso. Nova estrategia: %d\n", strategy);
+  return EXIT_SUCCESS;
 }
 
+int get_aloc_strategy(const char *strategy_string) {
+  if (strcmp(strategy_string, "first_fit") == 0) {
+    return FIRST_FIT;
+  } else if (strcmp(strategy_string, "worst_fit") == 0) {
+    return WORST_FIT;
+  } else if (strcmp(strategy_string, "best_fit") == 0) {
+    return BEST_FIT;
+  } else if (strcmp(strategy_string, "random_fit") == 0) {
+    return RANDOM_FIT;
+  }
+  return -1;
+}
